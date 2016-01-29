21f479360c7dd5c85f7c94e68d1543e25b848420
栋栋晓 <2243980491@qq.com>
2016-01-30 04:13:25 +0800

    Created Pages controller. Added Index and Explore pages. Changeed root URL to index page

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get explore" do
+    get :explore
+    assert_response :success
+  end
+
+end
